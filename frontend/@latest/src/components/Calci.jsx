2fdfc39e9b0deccf716e926d367c6efdaaa53e93
kapil kant <kapilkant471@gmail.com>
2024-01-09 15:00:53 +0530

    clci works perfectly need to add some style

--- frontend/@latest/src/components/Calci.jsx
+++ frontend/@latest/src/components/Calci.jsx
@@ -5,16 +5,18 @@
               <input type="text" id="input1" placeholder="Enter first number"/> <br />
               <input type="text" id="input2" placeholder="Enter second number"/> <br />
                   <button id="result">final result</button> <br />
-                 <button onClick={findAdd} >Add</button>
-                 {/* <button onClick={findSubtract} >Subtract</button>
-                 <button onClick={findMultiply} >Multiply</button>
-                 <button onClick={findDivide} >Divide</button>
-                 <button onClick={findModulo} >Modulo</button> */}
+                 <button onClick={findAdd} >Add</button> <br />
+                  <button onClick={findSubtract} >Subtract</button> <br />
+                 <button onClick={findMultiply} >Multiply</button> <br />
+                 <button onClick={findDivide} >Divide</button> <br />
+                 <button onClick={findModulo} >Modulo</button>  <br />
           </>
        )
    }
 
       function findAdd(){
+
+
         
             let input1=document.getElementById("input1").value;
             let input2=document.getElementById("input2").value;  
@@ -32,3 +34,75 @@
                     alert(e);
               }
       }
+
+     function findSubtract(){
+
+           let i1=document.getElementById("input1").value;
+           let i2=document.getElementById("input2").value;
+
+              try{
+
+                   fetch("http://localhost:3000/subtract?a="+i1+"&b="+i2)
+                   .then(async(res)=>{
+                        let data=  await res.json();
+                        document.getElementById("result").innerHTML=data;
+                   })
+
+              }catch(e){
+                    alert(e);
+              }
+     }
+     function findMultiply(){
+
+        let i1=document.getElementById("input1").value;
+        let i2=document.getElementById("input2").value;
+
+           try{
+
+                fetch("http://localhost:3000/multiply?a="+i1+"&b="+i2)
+                .then(async(res)=>{
+                     let data=  await res.json();
+                     document.getElementById("result").innerHTML=data;
+                })
+
+           }catch(e){
+                 alert(e);
+           }
+
+     }
+     function findDivide(){
+
+        let i1=document.getElementById("input1").value;
+        let i2=document.getElementById("input2").value;
+
+           try{
+
+                fetch("http://localhost:3000/divide?a="+i1+"&b="+i2)
+                .then(async(res)=>{
+                     let data=  await res.json();
+                     document.getElementById("result").innerHTML=data;
+                })
+
+           }catch(e){
+                 alert(e);
+           }
+
+     }
+     function findModulo(){
+
+        let i1=document.getElementById("input1").value;
+        let i2=document.getElementById("input2").value;
+
+           try{
+
+                fetch("http://localhost:3000/modulo?a="+i1+"&b="+i2)
+                .then(async(res)=>{
+                     let data=  await res.json();
+                     document.getElementById("result").innerHTML=data;
+                })
+
+           }catch(e){
+                 alert(e);
+           }
+
+     }
